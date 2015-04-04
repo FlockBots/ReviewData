835c7d798cf9060c208e7b772b46f8eefa0a2493
Stan Janssen <stanjanssen@outlook.com>
2015-04-04 17:20:27 +0200

    download archive first

--- side_scripts/submission_to_redis.py
+++ side_scripts/submission_to_redis.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     redis = Redis()
     reddit = Reddit('Whisky Archive Parser by /u/FlockOnFire')
     archive_parser = Parser('archive.csv')
-
+    archive_parser.download('1X1HTxkI6SqsdpNSkSSivMzpxNT-oeTbjFFDdEkXD30o')
     counter = 0
     for submission in archive_parser.get_submissions(skip=2000):
         if counter % 500 == 0:
